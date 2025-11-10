c61bdcd7e6bc917e32081b4e68ac5a4ff0502b43
Christopher Howard <124425557+howardc830@users.noreply.github.com>
2025-11-10 12:56:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/basic_cleaning",
+                "https://github.com/udacity/Project-Build-an-ML-Pipeline-Starter.git#src/basic_cleaning",
                 entry_point="main",
                 version="main",
                 env_manager="conda",
