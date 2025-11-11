d3367c3890bf9d78725dda83a5358a9b8c2b8d18
Christopher Howard <124425557+howardc830@users.noreply.github.com>
2025-11-11 16:55:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -93,11 +93,13 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 'main',
                 parameters = {
-
-                }
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]{"stratify_by"],
+                },
             )
            
-            pass
 
         if "train_random_forest" in active_steps:
 
